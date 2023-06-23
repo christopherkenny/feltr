74ccfe898ba312f8669f4835060899382307500e
Christopher Kenny <ctk44@cornell.edu>
2023-06-23 16:40:25 -0400

    finish user

--- R/user.R
+++ R/user.R
@@ -1,9 +1,23 @@
+#' Obtain information about the user
+#'
+#' @return a `tibble` of information about the user
+#' @export
+#'
+#' @examplesIf has_felt_key()
+#' felt_get_user()
 felt_get_user <- function() {
   req <- httr2::request(base_url = api_url()) |>
     httr2::req_url_path_append('user') |>
     httr2::req_auth_bearer_token(token = get_felt_key())
 
-  req |>
+  out <- req |>
     httr2::req_perform() |>
     httr2::resp_body_json()
+
+  tibble::tibble(
+    email = out$data$attributes$email,
+    name = out$data$attributes$name,
+    id = out$data$id,
+    type = out$data$type
+  )
 }
