6b8a693d685180bf5fb4052769aff85bea98eb35
Christopher Kenny <ctk44@cornell.edu>
2023-06-24 14:52:54 -0400

    move files

--- R/map.R
+++ R/map.R
@@ -1,5 +1,9 @@
 #' Get Map Information from Map ID
 #'
+#' - `felt_get_map()` returns identifying information for the map
+#' - `felt_get_map_layers()` returns information about each layer in the map
+#' - `felt_get_map_elements()` returns the shapes for each layer in the map
+#'
 #' @param map_id map identifier from url, from `https://felt.com/map/Map-ID`
 #'
 #' @return a [tibble::tibble] for the map
@@ -18,6 +22,35 @@ felt_get_map <- function(map_id) {
     proc_map()
 }
 
+
+#' @rdname felt_get_map
+#' @export
+felt_get_map_elements <- function(map_id) {
+  req <- httr2::request(base_url = api_url()) |>
+    httr2::req_url_path_append('maps', map_id, 'elements') |>
+    httr2::req_auth_bearer_token(token = get_felt_key())
+
+  req |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+}
+
+
+#' @rdname felt_get_map
+#' @export
+felt_get_map_layers <- function(map_id) {
+  req <- httr2::request(base_url = api_url()) |>
+    httr2::req_url_path_append('maps', map_id, 'layers') |>
+    httr2::req_auth_bearer_token(token = get_felt_key())
+
+  req |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+}
+
+
+
+
 proc_map <- function(l) {
   tibble::tibble(
     title = l$data$attributes$title,
@@ -27,3 +60,16 @@ proc_map <- function(l) {
     type = l$data$type
   )
 }
+
+proc_map_layer <- function(l) {
+  tibble::tibble(
+    name = purrr::map_chr(l$data, .f = function(x) purrr::pluck(x, 'attributes', 'name')),
+    progress = purrr::map_int(l$data, .f = function(x) purrr::pluck(x, 'attributes', 'progress')),
+    status = purrr::map_chr(l$data, .f = function(x) purrr::pluck(x, 'attributes', 'status')),
+    id = purrr::map_chr(l$data, 'id'),
+    relationships = purrr::map(l$data, .f = function(x) purrr::pluck(x, 'relationships')),
+    type = purrr::map_chr(l$data, 'type'),
+  )
+}
+
+
