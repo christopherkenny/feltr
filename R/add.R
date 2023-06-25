8d7b3a98d0d3870162057b69696e178881171cf9
Christopher Kenny <ctk44@cornell.edu>
2023-06-25 00:07:43 -0400

    up

---
+++ R/add.R
@@ -0,0 +1,3 @@
+felt_add_map_layers <- function(map_id, name, file_names, fill_color, stroke_color, webhook_url = NULL) {
+
+}
--- R/map.R
+++ R/map.R
@@ -3,6 +3,8 @@
 #' - `felt_get_map()` returns identifying information for the map
 #' - `felt_get_map_layers()` returns information about each layer in the map
 #' - `felt_get_map_elements()` returns the shapes for each layer in the map
+#' - `felt_get_map_geojson()` downloads a geojson version of the map and returns the path
+#' - `felt_get_map_sf` returns an `sf` version of the map
 #'
 #' @param map_id map identifier from url, from `https://felt.com/map/Map-ID`
 #'
@@ -11,6 +13,10 @@
 #'
 #' @examplesIf has_felt_key()
 #' felt_get_map('Rockland-2024-Districts-TBI8sDkmQjuK2GX9CSiHiUA')
+#' felt_get_map_layers('Rockland-2024-Districts-TBI8sDkmQjuK2GX9CSiHiUA')
+#' \donttest{
+#' felt_get_map_elements('Rockland-2024-Districts-TBI8sDkmQjuK2GX9CSiHiUA')
+#' }
 felt_get_map <- function(map_id) {
   req <- httr2::request(base_url = api_url()) |>
     httr2::req_url_path_append('maps', map_id) |>
@@ -45,11 +51,16 @@ felt_get_map_layers <- function(map_id) {
 
   req |>
     httr2::req_perform() |>
-    httr2::resp_body_json()
+    httr2::resp_body_json() |>
+    proc_map_layer()
 }
 
-
-
+#' @rdname felt_get_map
+#' @export
+felt_get_map_geojson <- function(map_id, path = tempfile(fileext = '.geojson')) {
+  curl::curl_download(paste0('https://felt.com/map/', map_id, '.geojson'),
+                      destfile = path)
+}
 
 proc_map <- function(l) {
   tibble::tibble(
@@ -67,9 +78,16 @@ proc_map_layer <- function(l) {
     progress = purrr::map_int(l$data, .f = function(x) purrr::pluck(x, 'attributes', 'progress')),
     status = purrr::map_chr(l$data, .f = function(x) purrr::pluck(x, 'attributes', 'status')),
     id = purrr::map_chr(l$data, 'id'),
-    relationships = purrr::map(l$data, .f = function(x) purrr::pluck(x, 'relationships')),
+    relationships = purrr::map(l$data, .f = function(x) purrr::pluck(x, 'relationships', 'datasets') |>
+                                 proc_relationships()),
     type = purrr::map_chr(l$data, 'type'),
   )
 }
 
-
+proc_relationships <- function(l) {
+  tibble::tibble(
+    name = purrr::map_chr(l$data, .f = function(x) purrr::pluck(x, 'attributes', 'name')),
+    id = purrr::map_chr(l$data, 'id'),
+    type = purrr::map_chr(l$data, 'type'),
+  )
+}
