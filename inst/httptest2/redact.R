d2a5caff7800744dc476a379a651cc170e911bf6
Christopher Kenny <ctk44@cornell.edu>
2024-06-02 21:08:48 -0400

    reset tests

--- inst/httptest2/redact.R
+++ inst/httptest2/redact.R
@@ -1,4 +1,7 @@
 function(resp) {
   resp |>
+    # shorten url
+    httptest2::gsub_response(pattern = 'felt.com/api/v2/', '') |>
+    # redact key
     httptest2::gsub_response(pattern = feltr::get_felt_key(), '')
 }
